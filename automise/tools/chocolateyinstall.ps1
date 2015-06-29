e4e72c7677cca1dee4144fece41e51097d91c3c3
Jason Duffy Smith <jason.nevar.smith@gmail.com>
2015-06-29 15:39:54 +1000

    Added nuspecs for Automise, AutomiseRunner, and ATCMD
    Added nuspecs for Automise, AutomiseRunner, and ATCMD. Next is to update
    them so that the version number can be changed on the fly.

---
+++ automise/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; #Stop on all errors
+
+$packageName = 'automise' #Unique name of the package
+$installerType = 'EXE' #Installer is an executable. 
+$url = 'http://downloads.automise.com/downloads/automise/400/AT400_852.exe' #URL to download installer for current version. 
+$silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
+$validExitCodes = @(0) #Valid return codes from the installer for success
+
+#Run chocolatey install package commandlet
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ automisecmd/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; #Stop on all errors
+
+$packageName = 'automisecmd' #Unique name of the package
+$installerType = 'EXE' #Installer is an executable. 
+$url = 'http://downloads.automise.com/downloads/automise/400/ATCMD400_852.exe' #URL to download installer for current version. 
+$silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
+$validExitCodes = @(0) #Valid return codes from the installer for success
+
+#Run chocolatey install package commandlet
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
