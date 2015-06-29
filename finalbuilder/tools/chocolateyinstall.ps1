e2132f6f10813154a237f2a7cba5235c954dc21a
Jason Duffy Smith <jason.nevar.smith@gmail.com>
2015-06-29 14:28:20 +1000

    FinalBuilder specification initial check in
    This checkin contains a fully working nuspec for the 7.0.0.3352 that was
    uploaded to chocolatey.org. Needs to be updated so that its a template
    where the version number is easily updated.

---
+++ finalbuilder/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop'; #Stop on all errors
+
+$packageName = 'finalbuilder' #Unique name of the package
+$installerType = 'EXE' #Installer is an executable. 
+$url = 'http://downloads.finalbuilder.com/downloads/finalbuilder/700/FB700_3352.exe' #URL to download installer for current version. 
+$silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
+$validExitCodes = @(0) #Valid return codes from the installer for success
+
+#Run chocolatey install package commandlet
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
---
+++ finalbuilder/tools/chocolateyuninstall.ps1
@@ -0,0 +1,32 @@
+﻿$ErrorActionPreference = 'Stop'; #Stop on all errors
+
+$packageName = 'finalbuilder' #Unique name of the package
+$registryUninstallerKeyName = '{4594DEE8-EFDC-4F16-A6DC-8FEAD022DC08}_is1' #The uninstall registry key for our application. Defined and setup by the installer exe.
+$installerType = 'EXE' #Our uninstaller is also an executable
+$silentArgs = '/SILENT /VERYSILENT /SUPPRESSMSGBOXES' #Silent arguments to make the uninstaller not show any GUI
+$validExitCodes = @(0) #Valid return codes from the uninstaller for success
+
+$shouldUninstall = $true; #We should uninstall if we detect no errors. 
+ 
+#Keys where our uninstall data is stored in registry
+$local_key       = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$local_key6432   = "HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key     = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+#Try each key. The first to return a valid path, and key get the UninstallString value from it. 
+$file = @($local_key, $local_key6432, $machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty UninstallString
+
+#If we don't have a valid uninstall executable, then error and don't run uninstall. 
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+	
+#If we are still uninstalling then call chocolatey uninstall package commandlet to run our uninstaller
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+}
