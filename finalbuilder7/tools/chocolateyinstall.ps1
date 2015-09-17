7755f5ece0187d43b6c67d19181bbb96ebd7ee98
Jason Duffy Smith <jason.nevar.smith@gmail.com>
2015-09-17 14:44:16 +1000

    Updated FinalBuilder 7 to work with build script
    Changed the download url and version to be easily replaced by the pack
    and publish script.

--- finalbuilder7/tools/chocolateyinstall.ps1
+++ finalbuilder7/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'finalbuilder' #Unique name of the package
 $installerType = 'EXE' #Installer is an executable. 
-$url = 'http://downloads.finalbuilder.com/downloads/finalbuilder/700/FB700_3450.exe' #URL to download installer for current version. 
+$url = '%PRODUCT_DOWNLOAD_URL%' #URL to download installer for current version. 
 $silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
 $validExitCodes = @(0) #Valid return codes from the installer for success
 
