ecba400c0a466f311d109e184ae38cb16c881c6f
Jason Duffy Smith <jason.nevar.smith@gmail.com>
2016-01-29 14:37:16 +1100

    Updating the package specs for Automise 4

--- automiserunner4/tools/chocolateyinstall.ps1
+++ automiserunner4/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName = 'automiserunner' #Unique name of the package
 $installerType = 'EXE' #Installer is an executable. 
-$url = 'http://downloads.automise.com/downloads/automise/400/AutomiseRunner400_852.exe' #URL to download installer for current version. 
+$url = '%PRODUCT_DOWNLOAD_URL%' #URL to download installer for current version. 
 $silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
 $validExitCodes = @(0) #Valid return codes from the installer for success
 
--- automiserunner4/tools/chocolateyuninstall.ps1
+++ automiserunner4/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; #Stop on all errors
 
 $packageName = 'automiserunner' #Unique name of the package
-$registryUninstallerKeyName = '{F728B6D7-2298-418C-8BF2-96C41C962663}_is1' #The uninstall registry key for our application. Defined and setup by the installer exe.
+$registryUninstallerKeyName = '{F5210620-068F-49C4-8C49-61C3D1A11FDD}_is1' #The uninstall registry key for our application. Defined and setup by the installer exe.
 $installerType = 'EXE' #Our uninstaller is also an executable
 $silentArgs = '/SILENT /VERYSILENT /SUPPRESSMSGBOXES' #Silent arguments to make the uninstaller not show any GUI
 $validExitCodes = @(0) #Valid return codes from the uninstaller for success
