e17d229fa3f752e2fb8c889e7261c97cba6f2823
Jason Duffy Smith <jason.nevar.smith@gmail.com>
2016-08-19 16:01:39 +1000

    Adding checksum to the installer script for Automise
    Added the checksum to the installer script for Automise.

--- automise5/tools/chocolateyinstall.ps1
+++ automise5/tools/chocolateyinstall.ps1
@@ -5,6 +5,7 @@ $installerType = 'EXE' #Installer is an executable.
 $url = '%PRODUCT_DOWNLOAD_URL%' #URL to download installer for current version. 
 $silentArgs = '/SP /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS' #Silent install arguments for our installer
 $validExitCodes = @(0) #Valid return codes from the installer for success
+$checksum = '%RELEASE_SHA256_CHECKSUM%'
 
 #Run chocolatey install package commandlet
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes -checksum $checksum -checksumType sha256
